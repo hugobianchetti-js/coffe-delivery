d48d312ba481ba8a84b14f805528181d4c5b0546
bianchettihugo <lhbianchetti@gmail.com>
2023-12-11 17:16:05 -0300

    setting up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { globalStyles } from "./styles/global"
+import { BaseLayout } from "./layouts/BaseLayout";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
 export default function App() {
   globalStyles();
 
   return (
-    <div></div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BaseLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
